e0fcc254b0a22ade6e3874c4fe4908dfc80f5de1
manit <manitshegde@gmail.com>
2024-02-29 21:00:44 +0530

    edit and delete action, partial views usages and toastr noti

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -19,6 +19,8 @@ namespace BulkyWeb.Controllers
             return View(objCategoryList);
         }
 
+
+        //create page
         public IActionResult Create() { // no action, it redirects to the Create.html page
             return View();
         }
@@ -27,14 +29,86 @@ namespace BulkyWeb.Controllers
         [HttpPost]
         public IActionResult Create(Category obj) //the action is performed in the category model
         {
+            //if(obj.Name == obj.DisplayOrder.ToString())
+            //{
+            //    ModelState.AddModelError("Name", "name cannot exactly match with Display Order");
+            //}
             if (ModelState.IsValid) // Checks the category 'obj' is valid, and examins all the data annotation(/checks validation) of the Category model and returns bool
             {
                 _db.Categories.Add(obj); // add the content from the create category form into the db
                 _db.SaveChanges(); // the EFC will save the changes done
+                TempData["Success"] = "Category created successfully"; //key value pair for the temp-data, which can be used for the next rendered page
                 return RedirectToAction("Index", "Category");
             }
             return View();   
             
         }
-    }
+
+        //Edit Page
+        public IActionResult Edit(int? id)
+        {
+            if (id == 0 || id == null)
+                return NotFound();
+
+            Category? categoryFromDb1 = _db.Categories.Find(id); // can only work with PK 
+            //Category? categoryFromDb2 = _db.Categories.FirstOrDefault(u=>u.Id == id); // can also search on Non-PK //
+            //Category? categoryFromDb3 = _db.Categories.Where(u=>u.Id==id).FirstOrDefault();
+
+
+            if (categoryFromDb1 == null)
+            {
+                return NotFound();
+            }
+            
+            return View(categoryFromDb1);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj) 
+        // if the 'id' of current obj is zero it is due to different Id name, either change it to -> 'Id'
+        // or add a tag <input asp-for="Id-name" hidden/> is required in html of that page 
+        {
+            
+            if (ModelState.IsValid) 
+            {
+                _db.Categories.Update(obj); // updates the content of the 'obj' passed in the database (Id shouldn't be zero, else EFC will add a new record)
+                _db.SaveChanges(); // the EFC will save the changes done
+                TempData["Success"] = "Category updated successfully";
+                return RedirectToAction("Index", "Category");
+            }
+            return View();
+
+        }
+
+		//Delete action
+
+		public IActionResult Delete(int? id)
+		{
+			if (id == 0 || id == null)
+				return NotFound();
+
+			Category? categoryFromDb1 = _db.Categories.Find(id); // can only work with PK 
+			if (categoryFromDb1 == null)
+			{
+				return NotFound();
+			}
+
+			return View(categoryFromDb1);
+		}
+
+		[HttpPost, ActionName("Delete")] //setting the name of this method explicitly as the name is changed to DeletePOST
+		public IActionResult DeletePOST(int? id)
+		{
+            Category obj = _db.Categories.Find(id);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+			_db.SaveChanges(); // the EFC will save the changes done
+			TempData["Success"] = "Category deleted successfully"; //key value pair for the temp-data, which can be used for the next rendered page, will stay for one request
+			return RedirectToAction("Index", "Category");
+
+		}
+	}
 }
